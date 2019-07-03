4e7ff6fecb4e4880e6ab6c020a04a8d67f988ef0
Eloïse Isautier <eloise.isautier@hetic.net>
2019-07-03 16:56:08 +0200

    :tada: init

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import DatepickerComponent from './components/Datepicker.js'
+// import moment from 'moment'
+
+var Datepicker = Vue.extend({
+
+})
+
+/* eslint-disable no-new */ /* eslint-disable no-tabs */ /* eslint-disable indent */ /* eslint-disable eol-last */
+new Vue({
+	el: 'body',
+	components: {
+		datepicker: DatepickerComponent
+	}
+})
